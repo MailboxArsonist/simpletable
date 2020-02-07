4aca65ac5c21599ec13b195ac10aff23a13aa465
Matthew Dodd <matthew.dodd93@gmail.com>
2020-02-07 14:51:54 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const options = [{
+    field: 'name',
+    label: 'Enterprise',
+    sortable: false
+  },{
+    field: 'type',
+    label: 'Statut',
+    sortable: false,
+  },{
+    field: 'date',
+    label: 'Date',
+    sortable: false,
+  },{
+    field: 'commission',
+    label: 'Gains',
+    sortable: false,
+  }]
