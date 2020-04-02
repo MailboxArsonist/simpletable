8c0f6246079a096949850b103e5029294996ef53
Matthew Dodd <matthew.dodd93@gmail.com>
2020-04-02 19:10:43 +0200

    wip add new options

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 import SimpleTable from "./simpleTable.js"
 
 const head = [{
-    field: 'name',
-    label: 'Enterprise',
-    sortable: true,
-    classList: 'text-left'
-  },{
-    field: 'type',
-    label: 'Statut',
-    sortable: false,
-    classList: 'text-left'
-  },{
-    field: 'date',
-    label: 'Date',
-    sortable: true,
-    classList: 'text-right'
-  },{
-    field: 'commission',
-    label: 'Gains',
-    sortable: false,
-    classList: 'text-right'
-  }
+  field: 'name',
+  label: 'Enterprise',
+  sortable: false
+},{
+  field: 'type',
+  label: 'Statut',
+  sortable: false,
+},{
+  field: 'date',
+  label: 'Date',
+  sortable: true,
+  classList: 'text-right'
+},{
+  field: 'commission',
+  label: 'Gains',
+  sortable: false,
+  classList: 'text-right'
+}
 ];
 
 const options = {
-    table: '#table',
-    details: false,
-    pagination: 'buttons', // 'buttons,' 
-    url: 'google.com'
-}
+  table: '#table',
+  details: false,
+  noResults: {
+    message: 'There are no results',
+    classList: ['text-center']
+  },
+  pagination: true, // 'buttons,' 
+  url: 'google.com'
+};
 
 const table = new SimpleTable(options, head)
 
